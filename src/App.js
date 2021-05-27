1f8f23be3eb33e119e57db794a221c86395cf0e7
github username <github email address>
2021-05-27 18:01:19 +0100

    replaced titles with image covers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import jujutsu from './images/jujutsu.jpg'
+import jujutsu from './images/jujutsu.jpg';
+import onepiece from './images/onepiece.jpeg';
+import fireforce from './images/fireforce.jpeg';
+import naruto from './images/naruto.jpeg';
+import attackontitan from './images/attackontitan.jpeg';
+import myheroacademia from './images/myheroacademia.jpeg';
 import './App.css';
 
 class App extends Component{
@@ -7,17 +12,28 @@ class App extends Component{
     return (
       <div className="App">
         <h1> Welcome to AniFlix</h1>
-        <img src={jujutsu} alt='Jujutsu'/>
         <br /><br /><br />
         <div className='container'>
-          <div className='item'>One Piece</div>
-          <div className='item'>Fire Force</div>
-          <div className='item'>Jujutsu Kaisen</div>
+          <div className='item'>
+            <img src={jujutsu} alt='Jujutsu'/>
+          </div>
+          <div className='item'>
+            <img src={onepiece} alt='One Piece'/>
+          </div>
+          <div className='item'>
+            <img src={fireforce} alt='Fire Force'/>
+          </div>
         </div>
         <div className='container'>
-          <div className='item'>My Hero Academia</div>
-          <div className='item'>Attack on Titian</div>
-          <div className='item'>OverLord</div>
+          <div className='item'>
+            <img src={naruto} alt='Naruto'/>
+          </div>
+          <div className='item'>
+            <img src={myheroacademia} alt='My Hero Academia'/>
+          </div>
+          <div className='item'>
+            <img src={attackontitan} alt='Attack on Titan'/>
+          </div>
         </div>
       </div>
     );
