ac90e2eb9f72ce2cc323f723d62e8abdb6b84405
github username <github email address>
2021-05-28 13:35:04 +0100

    Added anime title overlay om hover-over css

--- src/App.js
+++ src/App.js
@@ -16,23 +16,41 @@ class App extends Component{
         <div className='container'>
           <div className='item'>
             <img src={jujutsu} alt='Jujutsu'/>
+            <div className='overlay'>
+              <h1>Jujutsu Kaisen</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={onepiece} alt='One Piece'/>
+            <div className='overlay'>
+              <h1>One Piece</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={fireforce} alt='Fire Force'/>
+            <div className='overlay'>
+              <h1>Fire Force</h1>
+            </div>
           </div>
         </div>
         <div className='container'>
           <div className='item'>
             <img src={naruto} alt='Naruto'/>
+            <div className='overlay'>
+              <h1>Naruto</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={myheroacademia} alt='My Hero Academia'/>
+            <div className='overlay'>
+              <h1>My Hero Academia</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={attackontitan} alt='Attack on Titan'/>
+            <div className='overlay'>
+              <h1>Attack on Titan</h1>
+            </div>
           </div>
         </div>
       </div>
