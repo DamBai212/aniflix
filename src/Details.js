9f300f208dc1047bc3839fc441b2fba74164a351
github username <github email address>
2021-06-01 11:09:18 +0100

    state introduced on details component

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Details() {
-    return (
-        <div>
-            <h1>Welcome to the details page, WIP :)</h1>
-            <Link to='/'>Back to home page</Link>
-        </div>
-    )
+export default class Details extends Component {
+    
+    constructor() {
+        super();
+        debugger;
+        this.state = {
+            welcomeMesssage: 'Welcome to the details page, WIP :)'
+        };
+    }
+
+    componentDidMount() {
+        debugger;
+        setTimeout(() => {
+            debugger;
+            this.setState({
+               welcomeMesssage: 'More content coming soon!!!!'
+            });   
+        }, 3000);
+    }
+    
+    
+    render() {
+        debugger;
+        return (
+            <div>
+                <h1>{this.state.welcomeMesssage}</h1>
+                <Link to='/'>Back to home page</Link>
+            </div>
+        )
+
+    }
+
 }
+
