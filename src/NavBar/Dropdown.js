276192d7a5724012caeffc62e3d83dad17d71af6
github username <github email address>
2021-06-22 11:24:44 +0100

    improved functionality on on nav bar drop down, minor bus still need to be worked out

--- src/NavBar/Dropdown.js
+++ src/NavBar/Dropdown.js
@@ -2,24 +2,25 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Dropdown.css";
 import getGallery from "../getGallery.js";
-import Anime from "../Anime.js";
+// import Anime from "../Anime.js";
+
 
 export default function Dropdown() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+
   return (
     <div>
       <ul
         onClick={handleClick}
         className={click ? "dropdown-menu clicked" : "dropdown-menu"}
       >
-        {getGallery.map((anime) => (
-          <li key={anime.id}>
-            <Link to="/:animeId" onClick={() => setClick(false)}>
-              <Anime id={anime.id} name={anime.name} />
-              {anime.name}
-            </Link>
+        {getGallery().map((prop) => (
+          <li key={prop.id}>
+              <Link className={prop.cName} to={`/${prop.id}`} onClick={() => setClick(false)}>
+                  {prop.name}
+              </Link>
           </li>
         ))}
       </ul>
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,52 +1,77 @@
-import React, {useState} from 'react'
-import Button from './Button.js'
-import { Link } from 'react-router-dom'
-import './NavBar.css'
-import Dropdown from './Dropdown.js'
-
+import React, { useState } from "react";
+import Button from "./Button.js";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import Dropdown from "./Dropdown.js";
 
 export default function NavBar() {
-    const [click, setClick] = useState(false);
-    const [dropdown, setDropdown] = useState(false)
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
-    const handleClick = () => setClick(!click);
-    const closeMoblieMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    return (
-        <React.Fragment>
-            <nav className='navbar'>
-                <Link to='/' className='navbar-logo'>
-                    AniFlix
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-                </div>
-                {/* line 24 drags out the menu with the shows listed */}
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick= {closeMoblieMenu}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/:animeId' className='nav-links' onClick= {closeMoblieMenu}>
-                            Animes <i className='fas fa-caret-down' />
-                        </Link>
-                        {dropdown && <Dropdown/>}
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/contacts' className='nav-links' onClick= {closeMoblieMenu}>
-                            Contacts
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/sign-up' className='nav-links' onClick= {closeMoblieMenu}>
-                            Sign up
-                        </Link>
-                    </li>
-                </ul>
-                <Button />
-            </nav>
-        </React.Fragment>
-    )
+  const onMouseEnter = () => {
+      if(window.innerWidth < 960) {
+          setDropdown(false)
+      } else {
+          setDropdown(true)
+      }
+  }
+
+  const onMouseLeave = () => {
+    if(window.innerWidth < 960) {
+        setDropdown(false)
+    } else {
+        setDropdown(false)
+    }
+}
+
+  return (
+    <React.Fragment>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo">
+          AniFlix
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        {/* line 24 drags out the menu with the shows listed */}
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item"
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}>
+            <Link
+              to="/:animeId"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Animes <i className="fas fa-caret-down" />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contacts"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Contacts
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/sign-up" className="nav-links" onClick={closeMobileMenu}>
+              Sign up
+            </Link>
+          </li>
+        </ul>
+        <Button />
+      </nav>
+    </React.Fragment>
+  );
 }
