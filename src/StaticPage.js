befb20b3aa5cd19a80812111b23c1a71feb35fdc
Damany Bailey <damanybailey@icloud.com>
2026-03-21 21:56:50 +0000

    style(routes): align static and error pages with the AniFlix visual system update StaticPage.js and NotFound.js bring secondary pages into the same glass panel and typography system

--- src/StaticPage.js
+++ src/StaticPage.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function StaticPage(props) {
-    return (
-        <div>
-            <h1>{props.title}</h1>
-            <p>{props.body}</p>
-            <Link to='/'>Back to home page</Link>
-        </div>
-    );
+  return (
+    <main className='content-page'>
+      <section className='content-panel'>
+        <p className='content-eyebrow'>AniFlix</p>
+        <h1>{props.title}</h1>
+        <p>{props.body}</p>
+        <Link className='content-link' to='/'>
+          Back to home page
+        </Link>
+      </section>
+    </main>
+  );
 }
